07d456e07cd4129a89148b5e36ec814817bd09b1
Jamie Bullock <jamie@jamiebullock.com>
2020-08-06 21:11:24 +0100

    Updates

---
+++ precache-manifest.d8122f959ab1005ce0cfae71a3c40c44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0d633930970f81626f008bab9ab016",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "59f1a1d14d71b9afe6df",
+    "url": "/react-todo-app/static/css/main.67c1c519.chunk.css"
+  },
+  {
+    "revision": "d84796d6c7288065fd95",
+    "url": "/react-todo-app/static/js/2.02d7e6ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-app/static/js/2.02d7e6ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f1a1d14d71b9afe6df",
+    "url": "/react-todo-app/static/js/main.2ecec1e4.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  }
+]);
